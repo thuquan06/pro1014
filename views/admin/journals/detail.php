88a6b540d41418d76d3bed430dcd7c28d83c7e2a
Tienhien109 <hienntph52213@gmail.com>
2025-12-15 15:59:05 +0700

    fixx

--- views/admin/journals/detail.php
+++ views/admin/journals/detail.php
@@ -28,9 +28,12 @@
           <p style="margin: 4px 0 0 0; font-size: 16px; font-weight: 600;">
             <?= htmlspecialchars($journal['ten_hdv'] ?? 'N/A') ?>
           </p>
-          <?php if ($journal['email_hdv']): ?>
-            <small style="color: var(--text-light);"><?= htmlspecialchars($journal['email_hdv']) ?></small>
+          <?php if (!empty($incident['email_hdv'])): ?>
+            <small style="color: var(--text-light);">
+              <?= htmlspecialchars($incident['email_hdv']) ?>
+            </small>
           <?php endif; ?>
+
         </div>
       </div>
     </div>
